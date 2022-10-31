11e3b0f8265087e19be0fe925763b8c34cc34911
Charles Kemp <c.kemp@unimelb.edu.au>
2022-11-01 06:22:58 +1100

    changed plot origin, image resolution

--- open3D/PriorSimulation.py
+++ open3D/PriorSimulation.py
@@ -26,29 +26,32 @@ furtherest_distance = 300
 
 surfaces = []  # store the surface objects to visualise in 3D
 
-render = o3d.visualization.rendering.OffscreenRenderer(640, 480)
+render = o3d.visualization.rendering.OffscreenRenderer(512, 512)
 mat = o3d.visualization.rendering.MaterialRecord()
 mat.shader = 'defaultLit'
 
 for i in range(num_surfaces):
     surface_size = random.uniform(smallest_surface, largest_surface)
 
-    coord_x = random.uniform(-view_width/2 + surface_size/2,
-                             view_width/2 - surface_size/2)
+    coord_x = random.uniform(-view_width/2, view_width/2)
     # sample the mid-bottom point of the rectangle
     # CK: for Yang and Purves the camera is at a height of 1.65 m. I think we should put the camera at the origin (height 0) and adjust the simulation so that the "ground" is at -1.65 m
-    coord_y = random.uniform(-human_height, view_height -
-                             human_height - surface_size/2)
+    coord_y = random.uniform(-human_height, view_height - human_height)
 
     distance = random.uniform(shortest_distance, furtherest_distance)
+
+    # an average sized square that's sitting on the ground and as close as possible
+    #surface_size = 9
+    #coord_x = 0
+    #coord_y = -human_height
+    #distance = 2.5
 
     surface = o3d.geometry.TriangleMesh.create_box(
         width=surface_size, height=surface_size, depth=surface_thickness)
     surface.compute_vertex_normals()
-    surface.translate([coord_x, coord_y, distance])
+    surface.translate([coord_x - surface_size/2, coord_y, distance])
     render.scene.add_geometry("surface"+str(i), surface, mat)
     surfaces.append(surface)
-
 
 # projected simulation in 2D
 width = 512
@@ -56,6 +59,7 @@ height = 512
 
 # Camera Settings - CHANGE HERE
 f = 256
+f = 128
 px = 256
 py = 256
 
@@ -84,9 +88,9 @@ cimg = render.render_to_image()
 dimg = render.render_to_depth_image()
 
 plt.subplot(1, 2, 1)
-plt.imshow(cimg)
+plt.imshow(cimg, origin = "lower")
 plt.subplot(1, 2, 2)
-plt.imshow(dimg)
+plt.imshow(dimg, origin = "lower")
 plt.show()
 
 # simulation in 3D - it will take some time, uncomment it to see 3D simulation
