e4440442991e80250ed7f279b12d95047eec5cb7
Alun-XWL <Alun-XWL@outlook.com>
2022-12-21 16:34:10 +1100

    File Organisation

--- Open3D Simulation/IterativeSimulation.py
+++
@@ -1,9 +0,0 @@
-from subprocess import call
-import sys
-
-
-# As headless rendering is not supported and the visualisation window be iteratively opened
-iteration = 800
-for i in range(iteration):
-    print(i)
-    call(["python", "open3D/PriorSimulation.py"])
--- Open3D Simulation/test.py
+++
@@ -1,6 +0,0 @@
-import math
-import numpy as np
-axis = vector(0, 1, 0)
-theta = 1.2
-
-axis / math.sqrt(np.dot(axis, axis))
